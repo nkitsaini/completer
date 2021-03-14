20cf2ebcb34556b040e9b76f18f7608e8dc38ffc
ankit <nnkitsaini@gmail.com>
2021-03-14 20:17:14 +0530

    making 0 dependency library, jif updates

--- fake_completers/jif.py
+++ fake_completers/jif.py
@@ -1,7 +1,20 @@
 from complete import Command, Argument
 command = Command("jif", sub_commands=[
 	Command("git", sub_commands=[
+		Command("fastcommit"),
+		Command("mr-start"),
+		Command("mr-finish"),
+		Command("mr-reply"),
 		Command("latest"),
 		Command("fetch"),
-		Command("remove-all-changes")])])
+		Command("remove-all-changes"),
+		Command("remove-untracked-changes"),
+		Command("stage"),
+		Command("unstage"),
+		Command("delete-branch"),
+		Command("rename-branch"),
+		Command("latest"),
+		Command("fetch"),
+		Command("fetch"),
+		])])
 command.run()
